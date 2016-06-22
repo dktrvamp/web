ec3242d105b80fbc67acbd8accc7d1b7d5b8d73d
John S Oprisor <john.oprisor@dishdigital.com>
2016-06-22 00:59:00 -0600

    Updated the php file for sending email.

--- php/mail.php
+++ php/mail.php
@@ -1,23 +1,20 @@
 <?php
+print 'yoo it works';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-
-/*
-===================== perform check if any field in form is empty
- */
-if(empty($name) || empty($email) || empty($message) || empty($subject)){
-    echo "Kindly fill all the fileds";
-    die();
-}
-// change the following statement according to your need.
 $to = "drvaudio@gmail.com";
 
-if(mail($to, $subject, $message)){
-    echo "Message Sent!";
+
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+if (isset($_POST['submit'])) {
+    if (mail($to, $subject, $body)) {
+        echo '<p>Your message has been sent!</p>';
+    } else {
+        echo '<p>Something went wrong, go back and try again!</p>';
+    }
 }
-else {
-    echo "Failed!";
-}
+
 ?>
