f20aebe7b52e72fd390ea69b239a6b389baac821
John S Oprisor <john.oprisor@dishdigital.com>
2016-07-01 01:07:05 -0600

    Added service to fetch and parse rss feeds.

---
+++ service/rssFeed.js
@@ -0,0 +1,26 @@
+/**
+ * @ngdoc service
+ * @name ArtistInvoice
+ * @requires $state
+ * @requires $timeout
+ * @requires Utils
+ * @description
+ *
+ *
+ */
+angular.module("Dktrvamp").service("FeedService", function($http, $q){
+    "use strict";
+    return {
+        parseFeed: function(url){
+            var dfd = $q.defer(),
+                google_apis = "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=";
+            $http.jsonp( google_apis+ encodeURIComponent(url))
+            .then(dfd.resolve)
+            .catch(function(er) {
+                dfd.reject(er);
+            });
+
+            return dfd.promise;
+        }
+    };
+});
