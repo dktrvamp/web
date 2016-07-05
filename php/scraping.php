21f0bb5cd3e2c6cd34e4ad583c63d9ba8385cc0f
John S Oprisor <john.oprisor@dishdigital.com>
2016-07-05 00:38:57 -0600

    Scrapping website request.

--- php/scraping.php
+++ php/scraping.php
@@ -1,9 +1,9 @@
 <?php
-$response = array( 'success' => false );
-$sentdata = file_get_contents( 'php://input' );
-$data = json_decode( $sentdata );
-$url = $sentdata->url;
-$html = file_get_contents( $url );
+// $response = array( 'success' => false );
+// $sentdata = file_get_contents( 'php://input' );
+// $data = json_decode( $sentdata );
+// $url = $sentdata->url;
+$html = file_get_contents( "http://www.youredm.com/2016/07/04/stream-aphex-twins-cheetah-ep-full-via-bbc-radio-6" );
 
 $dom = new domDocument;
 $dom->loadHTML($html);
