9bb2068e11950dab9fb9317761c6cf5a0bc9b72e
John S Oprisor <john.oprisor@dishdigital.com>
2016-07-05 00:46:16 -0600

    Updated the scraping php file.

--- php/scraping.php
+++ php/scraping.php
@@ -1,5 +1,5 @@
 <?php
-$html = file_get_contents('"http://www.youredm.com/2016/07/04/stream-aphex-twins-cheetah-ep-full-via-bbc-radio-6"');
+$html = file_get_contents('http://www.youredm.com/2016/07/04/stream-aphex-twins-cheetah-ep-full-via-bbc-radio-6');
 $dom = new domDocument;
 $dom->loadHTML($html);
 $dom->preserveWhiteSpace = false;
@@ -7,4 +7,6 @@ $images = $dom->getElementsByTagName('img');
 foreach ($images as $image) {
   echo $image->getAttribute('src');
 }
+echo $images;
+print "Get Any?";
 ?>
