fd46cdbb61c23f1f37323653ea07ce039b289612
John S Oprisor <john.oprisor@dishdigital.com>
2017-01-31 01:29:06 -0700

    Replaced the google api with rss2json.

--- service/rssFeed.js
+++ service/rssFeed.js
@@ -16,9 +16,18 @@ angular.module("Dktrvamp").service("FeedService", function($http, $q){
     //--------------------------------------------------------------------------
 
     this.parseFeed = function(url){
-        var dfd = $q.defer(),
-            google_apis = "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=";
-        $http.jsonp( google_apis+ encodeURIComponent(url))
+        var dfd = $q.defer();
+            // yahoo_apis = "//query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(url) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+            // google_apis = "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=";
+            // https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.youredm.com%2Ffeed%2F
+        // url = encodeURIComponent(url);
+        $http.get("https://api.rss2json.com/v1/api.json",{
+            params: {
+                rss_url: url,
+                api_key: "u0dir3grn8zpbsz1ljwduwtxo1sqn9cw1cpo4uhd"
+
+            }
+        })
         .then(dfd.resolve)
         .catch(function(er) {
             dfd.reject(er);
